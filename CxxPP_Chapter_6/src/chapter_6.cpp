e8f49dda67def880b580ff547fc52c3b510bc8a4
cgallant <github@gallant.cc>
2015-06-16 12:44:24 -0400

    Add chapter 6 main and header.

---
+++ CxxPP_Chapter_6/src/chapter_6.cpp
@@ -0,0 +1,14 @@
+/*
+ * chapter_6.cpp
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: Chad
+ */
+#include <iostream>
+#include "chapter_6.h"
+
+int main() {
+	using namespace std;
+	cout << "Chapter 6." << endl;
+	return 0;
+}
---
+++ CxxPP_Chapter_6/src/chapter_6.h
@@ -0,0 +1,17 @@
+/*
+ * chapter_6.h
+ *
+ *  Created on: Jun 16, 2015
+ *      Author: Chad
+ */
+
+#ifndef CHAPTER_6_H_
+#define CHAPTER_6_H_
+
+void lising_6_1(){
+
+}
+
+
+
+#endif /* CHAPTER_6_H_ */
